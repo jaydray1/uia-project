5fd6f56dadf1e3e193a5f1511b921393c8cb35fe
JM <jdreher15@gmail.com>
2017-07-20 15:28:29 -0600

    about us cards

--- server/server.js
+++ server/server.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const massive = require('massive');
-const config = require('../config.js');
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , cors = require('cors')
+    , massive = require('massive')
+    , config = require('../config.js')
+    , app = express();
 
-
-const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+// var db = massive.connectSync({
+//   connectionString: config.connectionString
+// })
 
 //*********Serves static build files to the front-end
 app.use(express.static(__dirname + "./../build"));
 
 //*********Massive allows you to create sql files to populate your DB with */
 massive(config.connectionString).then(dbInstance => app.set('db', dbInstance));
-
 
 
 
--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import Hamburger from '.././components/Hamburger.js';
+import Rob from '../img/robversaw.jpeg';
+import Jm from '../img/bizpic.jpg';
 
 const About = _ => {
     return (
         <div>
             <Hamburger></Hamburger>
-            <h1>About Us!</h1>
+
+        <div className="card">
+            <img src={Rob} alt="Rob Versaw" className="img-card" />
+            <div className="holder">
+                <h1>Rob Versaw</h1>
+                <p className="title">Director</p>
+                <p>Duke University, MBA</p>
+                <a href="https://www.linkedin.com/in/rob-versaw"><i className="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+
+        <div className="card">
+            <img src={Rob} alt="Rob Versaw" className="img-card" />
+            <div className="holder">
+                <h1>Rob Versaw</h1>
+                <p className="title">Director</p>
+                <p>Duke University, MBA</p>
+                <a href="https://www.linkedin.com/in/rob-versaw"><i className="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+
+        <div className="card">
+            <img src={Rob} alt="Rob Versaw" className="img-card" />
+            <div className="holder">
+                <h1>Rob Versaw</h1>
+                <p className="title">Director</p>
+                <p>Duke University, MBA</p>
+                <a href="https://www.linkedin.com/in/rob-versaw"><i className="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+
+        <div className="card">
+            <img src={Jm} alt="Jon-Michael Dreher" className="img-card" />
+            <div className="holder">
+                <h1>Jon-Michael Dreher</h1>
+                <p className="title">Web Developer</p>
+                <a href="https://www.linkedin.com/in/jonmichaeldreher"><i className="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+
+
         </div>
             
     )
