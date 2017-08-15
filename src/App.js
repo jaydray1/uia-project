3fa4201f3073bbfca4310d5903359d528e3a1c15
JM <jdreher15@gmail.com>
2017-08-15 13:23:08 -0600

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import HomeView from './views/HomeView.js'
 import About from './views/About.js'
@@ -16,14 +16,16 @@ class App extends Component {
   render() {
     return (
       <div>
-         <Switch>
-              <Route exact path="/" component={HomeView}/>
-               <Route path="/about" component={About}/>
-              <Route path="/contact" component={Contact}/>
-              <Route path="/donate" component={Donate}/> 
-              <Route path='/robversaw' component={RobView}/>
-              <Route path='/juliawhite' component={About}/>
-          </Switch>
+        <Router>
+          <Switch>
+                <Route exact path="/" component={HomeView}/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/donate" component={Donate}/> 
+                <Route path='/robversaw' component={RobView}/>
+                <Route path='/juliawhite' component={About}/>
+            </Switch>
+        </Router>
       </div>
     );
   }
